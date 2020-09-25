9b1ef882f353d1b608730b1aea851d0aa1d242fb
RobertZetterlund <robert.zetterlund@outlook.com>
2020-09-25 23:42:42 +0200

    unable to accurately read text of old planlosning

--- text-recog.py
+++ text-recog.py
@@ -3,11 +3,7 @@ import pytesseract as pyt
 import matplotlib.pyplot as plt
 import glob 
 
-
-
-img = cv2.imread("beligium.png")
-
-print(img)
+img = cv2.imread("downloads/file1450.png")
 
 text = pyt.image_to_string(img)
 
